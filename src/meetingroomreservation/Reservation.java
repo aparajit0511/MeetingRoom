e7af298a0433c9366c17fa9edd296c493b01840f
aparajit0511 <aparajit35@gmail.com>
2026-09-20 13:25:49 +0530

    serious design flaw

--- src/meetingroomreservation/Reservation.java
+++ src/meetingroomreservation/Reservation.java
@@ -1,6 +1,7 @@
 package meetingroomreservation;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class Reservation {
@@ -11,12 +12,16 @@ public class Reservation {
     private LocalTime startTime;
     private LocalTime endTime;
 
+    private ArrayList<Reservation> reservationList = new ArrayList<>();
+
     public Reservation(Room room, User user, LocalTime startTime, LocalTime endTime) {
         this.reservationId = UUID.randomUUID().toString();;
         this.room = room;
         this.user = user;
         this.startTime = startTime;
         this.endTime = endTime;
+
+        reservationList.add(this);
     }
 
     public String getReservationId() {
@@ -31,6 +36,10 @@ public class Reservation {
 
     public LocalTime getEndTime() {
         return endTime;
+    }
+
+    public ArrayList getReservationList(){
+        return this.reservationList;
     }
 
 }
